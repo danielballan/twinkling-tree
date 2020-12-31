2a66f9e32f642f71df77c7a08b488f7d5b96124a
Dan Allan <daniel.b.allan@gmail.com>
2020-12-31 19:58:55 +0000

    Orhcestration works.

--- orchestrate.py
+++ orchestrate.py
@@ -11,27 +11,43 @@ OFF = (0, 0, 0)
 CAMERA_HOST = "pi2"
 
 
-def log(message):
-    print(message, file=sys.stderr)
+def log(*message):
+    print(*message, file=sys.stderr)
 
 
 def main():
-    process = subprocess.Popen(["ssh", "pi2", "python", "twinkling-tree/capture.py"])
-    process.stdout.readline()  # Block until ready.
+    process = subprocess.Popen(
+        [
+            "ssh",
+             "-i",
+             "picamera-key",
+             "-t",  # unbuffered output
+             f"pi@{CAMERA_HOST}",
+             "twinkling-tree/venv/bin/python",
+             "twinkling-tree/capture.py"
+        ],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print("received", process.stdout.readline())  # Block until ready.
     try:
         pixels = neopixel.NeoPixel(board.D18, PIXEL_COUNT, brightness=1, auto_write=False)
         pixels.fill(OFF)
-            for i in range(PIXEL_COUNT):
-                pixels[i - 1] = OFF 
-                pixels[i] = ON
-                pixels.show()
-                time.sleep(0.05)
-                log(f"Requesting image {i}")
-                process.stdin(f"light-{i}.png")
-                process.stdout.readline()
-                log(f"Completed image {i}")
+        for i in range(3):
+            pixels[i - 1] = OFF
+            pixels[i] = ON
+            pixels.show()
+            time.sleep(0.05)
+            log(f"Requesting image {i}")
+            process.stdin.write(f"light-{i}.png\n".encode())
+            process.stdin.flush()
+            log("received from ssh", process.stdout.readline())
+            log(f"Completed image {i}")
         process.stdin.close()
     finally:
+        process.terminate()
+        process.wait()
         pixels.fill(OFF)
         pixels.show()
 
